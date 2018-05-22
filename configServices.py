fe60c9ab71eb9b35fb1aa94cc41e942c0dff8f5a
Sundar Krishnamurthy <sundar_k@hotmail.com>
2018-05-22 14:08:55 -0700

    Update python script

--- configServices.py
+++ configServices.py
@@ -1,21 +1,24 @@
-import os;
+import os
 
-dbServerName = "";
-dbUsername = "";
-dbPassword = "";
-dbName = "";
+# Add your DB connection details here
+
+dbServerName = ""
+dbUsername = ""
+dbPassword = ""
+dbName = ""
 
 with open("EmptyWindowsService.exe.config.out", "wt") as fout:
     with open("EmptyWindowsService.exe.config", "rt") as fin:
         for line in fin:
 
-            line = line.replace('\r\n', '\n');
+            newLine = line.replace('\r\n', '\n')
 
-            if ("$$MYSQL_ENDPOINT$$" in line)       : fout.write(line.replace("$$MYSQL_ENDPOINT$$", dbServerName));
-            elif ("$$MYSQL_LOGIN$$" in line)        : fout.write(line.replace("$$MYSQL_LOGIN$$", dbUsername));
-            elif ("$$MYSQL_PASSWORD$$" in line)     : fout.write(line.replace("$$MYSQL_PASSWORD$$", dbPassword));
-            elif ("$$MYSQL_DATABASE$$" in line)      : fout.write(line.replace("$$MYSQL_DATABASE$$", dbName));
-            else                                    : fout.write(line);
+            if ("$$MYSQL_ENDPOINT$$" in newLine)       : newLine = newLine.replace("$$MYSQL_ENDPOINT$$", dbServerName)
+            if ("$$MYSQL_LOGIN$$" in newLine)          : newLine = newLine.replace("$$MYSQL_LOGIN$$", dbUsername)
+            if ("$$MYSQL_PASSWORD$$" in newLine)       : newLine = newLine.replace("$$MYSQL_PASSWORD$$", dbPassword)
+            if ("$$MYSQL_DATABASE$$" in newLine)       : newLine = newLine.replace("$$MYSQL_DATABASE$$", dbName)
 
-os.remove("EmptyWindowsService.exe.config");
-os.rename("EmptyWindowsService.exe.config.out", "EmptyWindowsService.exe.config");
+            fout.write(newLine)
+
+os.remove("EmptyWindowsService.exe.config")
+os.rename("EmptyWindowsService.exe.config.out", "EmptyWindowsService.exe.config")
