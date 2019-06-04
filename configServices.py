5b58835e85c01f73f251e6d87fdad6fe98830402
Sundar Krishnamurthy <sundar@passion8cakes.com>
2019-06-04 13:20:41 -0700

    Update configServices.py

--- configServices.py
+++ configServices.py
@@ -11,7 +11,7 @@ with open("EmptyWindowsService.exe.config.out", "wt") as fout:
     with open("EmptyWindowsService.exe.config", "rt") as fin:
         for line in fin:
 
-            newLine = line.replace('\r\n', '\n')
+            newLine = line
 
             if ("$$MYSQL_ENDPOINT$$" in newLine)       : newLine = newLine.replace("$$MYSQL_ENDPOINT$$", dbServerName)
             if ("$$MYSQL_LOGIN$$" in newLine)          : newLine = newLine.replace("$$MYSQL_LOGIN$$", dbUsername)
